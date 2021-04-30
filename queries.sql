a2e60a3611a3747a7b6a0f594917f8eedf8f7092
Bayan-daux <77173800+Bayan-daux@users.noreply.github.com>
2021-04-30 11:03:25 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -79,6 +79,32 @@ FROM current_emp as ce
 LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
-ORDER BY de.dept_no;
+ORDER BY de.dept_no:
+
+-- Reorder current_emp_by_dept Table by dept.
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+
+-- Create Table for Employee Information 
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+SELECT * FROM emp_info;
+
+
 
 
